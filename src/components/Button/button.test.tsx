f6dfd085591d710a1d942dc0c64c98d71f8e276e
gleiMoura <gle.bar.mou@gmail.com>
2023-06-23 18:08:29 -0300

    :test_tube: test: Create all tests to button

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,12 +1,35 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import Button from ".";
 
 describe('<Button />', () => {
-    it("Should render the button with text 'Load more'", () => {
+    it("It should render the button with text 'Load more'", () => {
         render(<Button text="Load more" />);
 
         const button = screen.getByRole('button', { name: /load more/i })
 
         expect(button).toBeInTheDocument();
-    })
+    });
+
+    it("It should call function on Button Click", () => {
+        const fn = jest.fn();
+        render(<Button text="Load More" onClick={fn} />);
+
+        const button = screen.getByRole("button", { name: /load more/i });
+        fireEvent.click(button);
+        expect(fn).toHaveBeenCalledTimes(1);
+    });
+
+    it("It should be disabled when disabled is true", () => {
+        render(<Button text="Load More" disabled={true} />);
+
+        const button = screen.getByRole("button", { name: /load more/i });
+        expect(button).toBeDisabled();
+    });
+
+    it("It should be disabled when disabled is true", () => {
+        render(<Button text="Load More" disabled={false} />);
+
+        const button = screen.getByRole("button", { name: /load more/i });
+        expect(button).toBeEnabled();
+    });
 })
