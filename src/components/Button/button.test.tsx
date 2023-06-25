83a51bc1f903f1b9ee30f8878702f9bbbddd39b7
gleiMoura <gle.bar.mou@gmail.com>
2023-06-24 22:39:33 -0300

    :recycle: refactor: Putting all properties in Button test

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -3,7 +3,7 @@ import Button from ".";
 
 describe('<Button />', () => {
     it("It should render the button with text 'Load more'", () => {
-        render(<Button text="Load more" />);
+        render(<Button text="Load more" onClick={() => { }} disabled={false} />);
 
         const button = screen.getByRole('button', { name: /load more/i })
 
@@ -12,7 +12,7 @@ describe('<Button />', () => {
 
     it("It should call function on Button Click", () => {
         const fn = jest.fn();
-        render(<Button text="Load More" onClick={fn} />);
+        render(<Button text="Load More" onClick={fn} disabled={false} />);
 
         const button = screen.getByRole("button", { name: /load more/i });
         fireEvent.click(button);
@@ -20,14 +20,14 @@ describe('<Button />', () => {
     });
 
     it("It should be disabled when disabled is true", () => {
-        render(<Button text="Load More" disabled={true} />);
+        render(<Button text="Load More" onClick={() => { }} disabled />);
 
         const button = screen.getByRole("button", { name: /load more/i });
         expect(button).toBeDisabled();
     });
 
-    it("It should be disabled when disabled is true", () => {
-        render(<Button text="Load More" disabled={false} />);
+    it("It should be enabled when disabled is false", () => {
+        render(<Button text="Load More" onClick={() => { }} disabled={false} />);
 
         const button = screen.getByRole("button", { name: /load more/i });
         expect(button).toBeEnabled();
